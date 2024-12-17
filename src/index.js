1d7fe16bca0a5c6b7e6d447e4ef861735b2b4a71
jomote-florida <jomote@alumnatflorida.es>
2024-12-17 11:16:22 +0100

    Mejora de mensaje de consola

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 app.set("port", process.env.PORT || 8080);
 
 app.listen(app.get("port"), () => {
-    console.log("Hola desde el puerto "+ app.get("port"));
+    console.log("Te encuentras en el puerto "+ app.get("port"));
 });
 
 
