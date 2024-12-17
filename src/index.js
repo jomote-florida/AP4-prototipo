5cd335ee289bc45a9f741ace8a8951b83d8f989b
jomote-florida <jomote@alumnatflorida.es>
2024-12-17 11:09:54 +0100

    Creacion base de nuestro prototipo

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require ('express');
+const app = express();
+
+app.set("port", process.env.PORT || 8080);
+
+app.listen(app.get("port"), () => {
+    console.log("Hola desde el puerto "+ app.get("port"));
+});
+
+
+
+app.use(require("./routes/indexRoutes"));
---
+++ src/routes/indexRoutes.js
@@ -0,0 +1,24 @@
+const { Router } = require('express');
+const router = Router();
+
+router.get("/", (req,res) => {
+    res.send("Hola usuario, te encuentras en el indice general<br>Marque los siguientes parametros para acceder al resto de paginas creadas<br>/corredoresDelViento<br>/rompedoresDelCielo<br>/danzantesDelFilo<br>/escultoresDeVoluntad");
+})
+
+router.get("/corredoresDelViento", function (req,res) {
+    res.send("1º Ideal: Vida antes que muerte, fuerza antes que debilidad, viaje antes que destino. <br>2º Ideal: Protegeré a aquellos que no puedan protegerse a si mismos<br>3º Ideal: Protegeré incluso a quienes odie. Mientras sea lo justo.<br> 4º Ideal: Aceptaré que habrá a quienes no pueda proteger.");
+});
+
+router.get("/rompedoresDelCielo", function (req, res){
+    res.send("1º Ideal: Vida antes que muerte, fuerza antes que debilidad, viaje antes que destino. <br> 2º Ideal: Juro buscar la justicia y permitir que me guíe hasta que halle un ideal más perfecto. <br> 3º Ideal: Juro cumplir la voluntad de la ley. <br> 4º Ideal: Purgare el mal que nos atormenta, siempre que la ley lo permita.<br> 5º Ideal: Acepto mi verdad, acepto convertirme en la ley");
+});
+
+router.get("/danzantesDelFilo", function (req,res) {
+    res.send("1º Ideal: Vida antes que muerte, fuerza antes que debilidad, viaje antes que destino<br>2º Ideal: Recordaré a aquellos que han sido olvidados. <br>3º Ideal: Escuchare a aquellos que han sido ignorados");
+});
+
+router.get("/escultoresDeVoluntad", function (req,res) {
+    res.send("1º Ideal: Vida antes que muerte, fuerza antes que debilidad, viaje antes que destino<br>2º Ideal: Procuraré la libertad a quienes están atados");
+});
+
+module.exports = router;
